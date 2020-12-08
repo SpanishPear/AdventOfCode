5c52cb95d2271aed0206bb5462aea061ac4dbf1c
Shrey Sudhir <z5257343@ad.unsw.edu.au>
2020-12-08 18:06:38 +1100

    Update day06.py

--- day06/day06.py
+++ day06/day06.py
@@ -3,9 +3,10 @@ from functools import reduce
 import sys
 
 def partOne():
-    partOne = sum([len(list(dict.fromkeys(list(item.replace('\n',''))))) for item in sys.stdin.read().split('\n\n')])
-    print(partOne)
+    print(sum([len(list(dict.fromkeys(list(item.replace('\n',''))))) for item in sys.stdin.read().split('\n\n')]))
 
 def partTwo():
-    partTwo = sum([len(set.intersection(*[set(list(item)) for item in item.split()])) for item in sys.stdin.read().split('\n\n')]) 
-print(partTwo)
+    print(sum([len(set.intersection(*[set(list(item)) for item in item.split()])) for item in sys.stdin.read().split('\n\n')])) 
+
+partOne()
+partTwo()
