52e322940e7df9fc404aaafd7b6ced0cc281a511
Shrey Somaiya <ssomaiya@atlassian.com>
2021-12-01 17:43:32 +1100

    part 1

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -1,7 +1,22 @@
-pub fn part1(_input: &String) -> u32 {
-    todo!();
+
+pub fn part1(input: &String) -> u32 {
+
+    let lines: Vec<u32> = input.lines().map(|item| {
+        item.parse::<u32>().unwrap()
+    }).collect();
+
+    let mut sum =0;
+    for num in lines.windows(2) {
+        if let &[prev, next] = num {
+            if next > prev {
+                sum += 1;
+            }
+        }
+    }
+
+    sum
 }
 
 pub fn part2(_input: &String) -> u32 {
-    todo!();
+    4
 }
