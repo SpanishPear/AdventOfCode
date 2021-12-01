64332f36e38beb5aa04ae84f86fe7d0c1214f947
Shrey Somaiya <ssomaiya@atlassian.com>
2021-12-01 17:45:41 +1100

    yay part 2

--- 2021/src/day01.rs
+++ 2021/src/day01.rs
@@ -17,6 +17,19 @@ pub fn part1(input: &String) -> u32 {
     sum
 }
 
-pub fn part2(_input: &String) -> u32 {
-    4
+pub fn part2(input: &String) -> u32 {
+    let lines: Vec<u32> = input.lines().map(|item| {
+        item.parse::<u32>().unwrap()
+    }).collect();
+
+    let mut sum =0;
+    for num in lines.windows(4) {
+        if let &[first, second, third,fourth] = num {
+            if second + third + fourth > first + second + third {
+                sum += 1;
+            }
+        }
+    }
+
+    sum
 }
